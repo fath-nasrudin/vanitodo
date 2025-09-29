ef5c376c878990e032a3d75039870a946109f943
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-09-29 19:33:01 +0700

    Add todo model and crud functions

---
+++ js/model.js
@@ -0,0 +1,24 @@
+const state = {
+  todos: [],
+};
+
+export function getTodos() {
+  return todos;
+}
+
+export function addTodo(text) {
+  state.todos.push({
+    id: Date.now(),
+    text,
+    completed: false,
+  });
+}
+
+export function toggleTodo(id) {
+  const todo = state.todos.find((todo) => todo.id === id);
+  if (todo) todo.completed = !todo.completed;
+}
+
+export function deleteTodo(id) {
+  state.todos = state.todos.filter((todo) => todo.id !== id);
+}
