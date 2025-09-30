4e650b243952979b9d37ced5d968c8d8abbcb3de
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-09-30 19:16:22 +0700

    Add Todo components

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { getTodos } from "./js/model.js";
 function createEl(tag, props = {}) {
   const node = document.createElement(tag);
 
@@ -29,11 +30,100 @@ function createEl(tag, props = {}) {
   return node;
 }
 
+function TodoItem(todoData) {
+  const checkbox = createEl("input", {
+    type: "checkbox",
+    className: "grow-0 relative top-1",
+  });
+
+  const todoTitle = createEl("h3", {
+    text: todoData.title,
+    className: "text-sm",
+  });
+
+  const todoDescription = createEl("p", {
+    text: todoData.description,
+    className: "text-xs",
+  });
+
+  const todoFooter = createEl("div", {
+    className: "text-xs",
+    children: [
+      // dueDate
+      createEl("button", {
+        text: todoData.dueDate,
+      }),
+      // priority
+      createEl("button", {
+        text: todoData.priority,
+      }),
+    ],
+  });
+
+  return createEl("li", {
+    className: "relative border-b border-border flex gap-4 items-start py-2",
+    children: [
+      checkbox,
+      createEl("div", {
+        children: [todoTitle, todoDescription, todoFooter],
+      }),
+    ],
+  });
+}
+
+function TodoList() {
+  // const todos = getTodos();
+  const todos = [
+    {
+      title: "Membuat aplikasi todolist",
+      description: "Ini adalah descriptionnya untuk aplikasi ini",
+      dueDate: "27 Nov 2025",
+      priority: "P1",
+    },
+    {
+      title: "Membuat aplikasi todolist",
+      description: "Ini adalah descriptionnya untuk aplikasi ini",
+      dueDate: "27 Nov 2025",
+      priority: "P1",
+    },
+  ];
+  return createEl("ul", {
+    children: todos.map((item) => TodoItem(item)),
+  });
+}
+
+function TodoSection() {
+  return createEl("div", {
+    children: [
+      createEl("h2", {
+        text: "Overdue",
+        className: "font-bold border-b border-border",
+      }),
+      TodoList(),
+    ],
+  });
+}
+
+function App() {
+  // main container
+  const main = createEl("main", {
+    className: "mx-auto max-w-3xl",
+    children: [
+      // Title
+      createEl("h1", {
+        text: "Inbox",
+        className: "text-2xl font-bold",
+      }),
+      TodoSection(),
+    ],
+  });
+  return main;
+}
+
 function render() {
   const root = document.getElementById("app");
   root.innerHTML = "";
-  const header = createEl("h1", { text: "Hello World" });
-  root.appendChild(header);
+  root.appendChild(App());
 }
 
 // Initial render
